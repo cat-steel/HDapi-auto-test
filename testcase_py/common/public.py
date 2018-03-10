1a56e465cafd5595119bc9595c42347a3dcf3dd2
cat-steel <33481456+cat-steel@users.noreply.github.com>
2018-03-10 20:34:30 +0800

    添加注释

--- testcase_py/common/public.py
+++ testcase_py/common/public.py
@@ -1,16 +1,20 @@
 import os,xlrd,xlwt,time
 
+#通过配置文件里的接口名称来获取接口url的函数
 def get_url(api_name):
 	fp = open('D:\person\learn\py\HDapi\config\API_url.txt')
+#按行读取接口url配置文件
 	api_infos = fp.readlines()
 	fp.close()
-	api_urls = []
+#通过for循环来遍历配置文件里的每一个url，并且返回传入的接口名称相应的url
 	for api in api_infos:
+#去除因为读取产生的换行空格等
 		api_f = api.strip(' \r\n\t')
 		api_c = api_f.split('=')
 		if api_name == api_c[0]:
 			return api_c[1]
 
+#通过传入用例名称的文件和excel页面来读取测试用例
 def get_case(filename,sheetnum):
 	case_dir='D:\\person\\learn\\py\\HDapi\\testcase_excel' + '\\' + filename + '.xlsx'
 
@@ -20,15 +24,19 @@ def get_case(filename,sheetnum):
 	nol = table.ncols
 	return nor,table
 
+#通过xlwt库来设计测试报告并写入excel里面
 def write_report():
 	workbook = xlwt.Workbook(encoding='utf-8')
+#在excel测试报告表格中创建名叫housemanage的页面
 	worksheet = workbook.add_sheet('housemanage')
+#设置字体格式为居中对齐
 	alignment = xlwt.Alignment()
 	alignment.horz = alignment.HORZ_CENTER
 	alignment.vert = alignment.VERT_CENTER
 	style = xlwt.XFStyle()
 	style.alignment = alignment
-
+	
+#具体的合并哪些单元格并且写入相应的信息
 	worksheet.write_merge(0,0,0,7,'测试报告(housemanage)',style)
 	worksheet.write_merge(1,10,0,0,'house_manage',style)
 	worksheet.write_merge(1,2,1,1,'insethouse',style)
@@ -49,5 +57,6 @@ def write_report():
 	worksheet.write(10,2,'detail')
 	worksheet.write(1,12,'pass')
 	worksheet.write(1,13,'faild')
+#最后返回worksheet,workbook两个参数，因为在测试测试用例和运行文件中需要用到的两个参数
 	return worksheet,workbook
 
